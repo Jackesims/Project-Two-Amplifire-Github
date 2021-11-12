ffead158f78ff9679a9a862b17364d8668d88718
pstapleton1925 <92169330+pstapleton1925@users.noreply.github.com>
2021-11-12 10:52:52 -0500

    updating CSVDataset.scala

--- src/main/scala/example/CSVDataset.scala
+++ src/main/scala/example/CSVDataset.scala
@@ -1,7 +1,5 @@
 package example
 
-package example
-
 import java.io.IOException
 import scala.util.Try
 // import os._
@@ -14,7 +12,9 @@ import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 import java.io.PrintWriter;
-
+import java.io._
+import scala.collection.mutable.ListBuffer
+import scala.io.Source
 
 class CSVDataset(var filename: String) {
 
@@ -23,21 +23,18 @@ class CSVDataset(var filename: String) {
     def CheckFileExists(filename: String): Boolean = {
         println("Checking to see if file already exists")
         var CheckExistFlag = Files.exists(Paths.get(filename))
-        println(CheckExistFlag)
         return CheckExistFlag
     }
 
     def copyFromMariaDev(): Unit = {
         val src = "/home/maria_dev/" + filename
         var srcExistsFlag = CheckFileExists(src) 
-        println(srcExistsFlag)
         if(srcExistsFlag) {
 
             // If the file in question exists on /home/maria_dev, execute this block of code.
 
             println("File exists on maria_dev. Checking to see if file exists in target location.")
             var target = path + filename
-            println(target)
             var targetExistsFlag = CheckFileExists(target)
             if(!(targetExistsFlag)) {
 
@@ -48,7 +45,7 @@ class CSVDataset(var filename: String) {
                 val fs = FileSystem.get(conf)
                 val localpath = new Path("file://" + src)
                 val hdfspath = new Path(target)
-                fs.copyFromLocalFile(false, localpath, hdfspath)
+                fs.copyFromLocalFile(true, true, localpath, hdfspath)
                 println(s"Done copying local file $src to $target ...")
             } else {
             println("File in question already exists on HDFS. Moving on to next file.")
@@ -64,17 +61,53 @@ object Project2Code{
     // Put any global variables here.
 
     def main(args: Array[String]): Unit =  {
-        // Open the CSV file "csvFileList.csv", read the file, and make a list of strings representing .csv files
-        var CSVFileList = readFiletoList("csvFileList.csv")
-        // Iterate over the CSVFileList, and upload that file from /home/maria_dev to /user/maria_dev on HDFS.
+        // Open the CSV file "csvFileListTest.csv", read the file, and make a list of strings representing .csv files
+        var CSVFileList = readFiletoList("csvFileListTest.csv")
+        // Create ListBuffer object that serves as a list, but is mutable.
+        var listBufferCSVData = ListBuffer[String]()
+        var FileIterator = 0
+        // Iterate over the CSVFileList, and extract each line to listBufferCSVData. Also, don't include multiple headers, and append a last column to include the year.
         for(file <- CSVFileList) {
-            var CSVInstance = new CSVDataset(file)
-            CSVInstance.copyFromMariaDev()
+            var year = file.split("_")(0)
+            var LineIterator = 0
+            var bufferedSource = Source.fromFile(file)
+            for (line <- bufferedSource.getLines) {
+                if((LineIterator == 0) & (FileIterator==0)){
+                // If first line of CSV and first file to be accessed, include the line into the listBuffer. This is the CSV file header
+                    println(s"Merging $file")
+                    listBufferCSVData += line + ",year\n"
+                }else if((LineIterator == 0) & (FileIterator!=0)){
+                // If first line of CSV but not first file to be accessed, don't include the line into the listBuffer. This is a redundant header row
+                    println(s"Merging $file")
+                }else{
+                // If not first line of CSV, then it is a record entry. Include in listBuffer.
+                    listBufferCSVData += line + s",$year\n"
+                }
+                LineIterator += 1
+            }
+            bufferedSource.close
+            FileIterator += 1
+        // Convert the mutable listBuffer object into an immutable list
+        var listCSVData = listBufferCSVData.toList
+        // Create a CSV file representing all of the merged data
+        writeFile("Merged_CSV.csv", listCSVData)
+        var CSVInstance = new CSVDataset("Merged_CSV.csv")
+        // Move merged file to HDFS
+        CSVInstance.copyFromMariaDev()
         }
-        println("Starting Project 2 Demo")
     }
 
     // Global functions go here.
+
+    def writeFile(filename: String, lines: Seq[String]): Unit = {
+        // This function takes a list of strings, and writes them to a file.
+        val file = new File(filename)
+        val bw = new BufferedWriter(new FileWriter(file))
+        for (line <- lines) {
+            bw.write(line)
+        }
+        bw.close()
+    }
 
     def readFiletoList(filename: String): Seq[String] = {
         // This function opens any file which has lines delimited by returns (\n), and returns
