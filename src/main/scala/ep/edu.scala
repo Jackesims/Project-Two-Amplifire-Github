dce54126ed048972c55d958b601fbae38642b4d6
eperez176 <emanuel.perez@revature.net>
2021-11-15 16:18:48 -0600

    optimize query

--- src/main/scala/ep/edu.scala
+++ src/main/scala/ep/edu.scala
@@ -6,7 +6,7 @@ import org.apache.spark.sql.types._
 import org.apache.spark.sql.functions._
 import scala.collection.JavaConversions._
 
-object edu{
+object edu {
     // Program reads from hdfs, creates a df from it, and prints out top 10 rows in column style then prints top10 rows who are women
     val spark = SparkSession
       .builder()
@@ -16,26 +16,14 @@ object edu{
 
     import spark.implicits._
     spark.sparkContext.setLogLevel("ERROR")
-    val eduList = List("'1'","'2'","'3'","'4'","'5'","'6'","'7'","'8'");
-    val titleList = List("8th Grade Or Less","Some High School, No Diploma","High School Gradute or GED Completed","Some College Credit, But No Degree","Associate Degree","Bachelor's Degree","Master's Degree","Doctorate or Professional Degree");
     println("\nCreating the data Set...\n")
-    val parqDF2 = spark.read.parquet("/user/maria_dev/data_edu.parquet")
-
-    var avgAgeArray = Array.ofDim[Double](titleList.length,2);
-
-    var count = 0;
-
-    for(edu <- eduList) {
-        var res = parqDF2.filter("education_2003_revision = " + edu).filter("detail_age > 18").agg(avg("detail_age").as("Avg Age " + titleList(count))).collect();
-            
-        for(row <- res) {
-            println("Average Age of " + titleList(count))
-            avgAgeArray(count)(0) = row.getDouble(0)
-            println(avgAgeArray(count)(0));
-        }
-        avgAgeArray(count)(1) = parqDF2.filter("education_2003_revision = " + edu).filter("detail_age > 18").count();
-        println(avgAgeArray(count)(1))
-        count = count + 1;
-    }
-
+    val parqDF2 = spark.read.parquet("/user/maria_dev/merged_data.parquet")
+    parqDF2
+      .filter("detail_age > 18")
+      .groupBy("sex", "education_2003_revision")
+      .agg(
+        avg("detail_age").as("Avg Age"), 
+        count("*").alias("Total Number") )
+      .orderBy("sex","education_2003_revision")
+      .show();
 }
--- src/main/scala/ep/main.scala
+++ src/main/scala/ep/main.scala
@@ -6,7 +6,7 @@ import org.apache.spark.sql.types._
 import org.apache.spark.sql.functions._
 import scala.collection.JavaConversions._
 
-object Main {
+object Main extends App{
     // Program reads from hdfs, creates a df from it, and prints out top 10 rows in column style then prints top10 rows who are women
     val spark = SparkSession
       .builder()
@@ -16,14 +16,28 @@ object Main {
 
     import spark.implicits._
     spark.sparkContext.setLogLevel("ERROR")
-    val eduList = List("'1'","'2'","'3'","'4'","'5'","'6'","'7'","'8'");
-    val titleList = List("8th Grade Or Less","Some High School, No Diploma","High School Gradute or GED Completed","Some College Credit, But No Degree","Associate Degree","Bachelor's Degree","Master's Degree","Doctorate or Professional Degree");
-    println("\nCreating the data Set...\n")
-    val df1 = spark.read.parquet("/user/maria_dev/merged_data.parquet")
-    println("Data set created\n")
-    println("Creating edu parquet with partitions...")
-    df1.write.partitionBy("sex","education_2003_revision").parquet("/user/maria_dev/edu_data.parquet")
-    println("Read the paritioned parquet...")
-    println("\nDone!")
+    println("\nReading the data Set...\n")
+    val parqDF1 = spark.read.parquet("/user/maria_dev/merged_data.parquet")
+    println("Done...\n")
+    println("Doing the marital query...")
+    parqDF1
+      .filter("detail_age > 18")
+      .groupBy("sex", "marital_status")
+      .agg(
+        avg("detail_age").as("Avg Age"), 
+        count("*").alias("Total Number") )
+      .orderBy("sex","marital_status")
+      .show();
+    println("Doing Education Query...")
+    parqDF1
+      .filter("detail_age > 18")
+      .groupBy("sex", "education_2003_revision")
+      .agg(
+        avg("detail_age").as("Avg Age"), 
+        count("*").alias("Total Number") )
+      .orderBy("sex","education_2003_revision")
+      .show(40);
+  
+  println("DONE....\n")
 
 }
